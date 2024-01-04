c4458433e461accb76747e665474cf9d92f1ee32
Akshithre <107135431+Akshithre@users.noreply.github.com>
2024-01-04 15:41:27 +0530

    Update App.java

--- src/main/java/SEGroup/AkshithMavenJavaProject/App.java
+++ src/main/java/SEGroup/AkshithMavenJavaProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from Akshith" );
+        System.out.println( "Hello from Akshith hey" );
     }
 }
